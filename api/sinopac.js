544c68186d5c2ad45401c8c80640f53dec322ac4
yenhaochou <yenhaochou@gmail.com>
2026-06-21 12:32:13 +0800

    今日全部更新

--- api/sinopac.js
+++ api/sinopac.js
@@ -2,7 +2,7 @@
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin',  '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') { res.status(200).end(); return; }
 
